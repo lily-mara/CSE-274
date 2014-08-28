fffb7aa2c809b1c73b224bb889aeed5353e858d8
Nate Mara <natemara@gmail.com>
2014-08-28 10:47:41 -0400

    added info about pointer, reference, & cloning

--- LAB01/src/lab01.cpp
+++ LAB01/src/lab01.cpp
@@ -12,6 +12,30 @@ using namespace std;
 int main() {
 	Rectangle rect;
 	rect.set_values(3, 4);
-	cout << "area: " << rect.area();
+
+	// Java: Rectangle rect2 = rect.clone()
+	// clones object
+	Rectangle rect2 = rect;
+
+	// Java: Rectangle rectRef = rect
+	// creates a reference
+	Rectangle &rectRef = rect;
+
+	// same as `Rectangle *pRect = &rect;`
+	// creates a pointer
+	Rectangle *pRect;
+	pRect = &rect;
+
+	// use this syntax to create a new object on the heap
+	// by default, c++ objects are created on the stack
+	Rectangle *rectHeap = new Rectangle();
+
+	rect2.set_values(2, 3);
+	rectRef.set_values(1, 1);
+
+	cout << "area1: " << rect.area() << endl;
+	cout << "area2: " << rect2.area() << endl;
+	cout << "rect pointer: " << pRect << endl;
+	cout << "rect pointer area: " << pRect->area() << endl;
 	return 0;
 }
