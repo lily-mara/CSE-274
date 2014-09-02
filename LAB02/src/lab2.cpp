0d4ca6539818489d65be1e263f3532007f752dfc
Nate Mara <natemara@gmail.com>
2014-09-02 11:18:08 -0400

    added test code to main file

---
+++ LAB02/src/lab2.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : lab2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "fifo_queue.h"
+using namespace std;
+
+int main() {
+  FifoQueue queue;
+  queue.Enqueue('a');
+  queue.Enqueue('b');
+	return 0;
+}
