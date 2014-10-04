7b6d53482048993a33423c3aad97ec30d53451ce
Nate Mara <natemara@gmail.com>
2014-10-04 13:07:46 -0400

    fix IsPalendrome

--- assignment2/src/DLList.h
+++ assignment2/src/DLList.h
@@ -126,7 +126,7 @@ T DLList<T>::remove(int i) {
 template<class T>
 bool DLList<T>::IsPalendrome() {
   for (int i = 0; i < n/2; i++) {
-	if (get(i) != get(n-i)) {
+	if (get(i) != get(n-i-1)) {
 	  return false;
 	}
   }
