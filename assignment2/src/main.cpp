36c79a0064d0d057a99df06b3f07e4c15a78890a
Nate Mara <natemara@gmail.com>
2014-10-04 13:12:33 -0400

    wrote test cases for IsPalendrome

--- assignment2/src/main.cpp
+++ assignment2/src/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <assert.h>
 
 #include "DLList.h"
 
@@ -14,7 +15,30 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void testPalendrome();
+
 int main() {
-  DLList<int> x;
+  testPalendrome();
   return 0;
+}
+
+void testPalendrome() {
+  DLList<char> bob;
+  bob.add('b');
+  bob.add('o');
+  bob.add('b');
+
+  assert(bob.IsPalendrome());
+
+  DLList<int> nums;
+  nums.add(0);
+  nums.add(1);
+  nums.add(2);
+  nums.add(3);
+  nums.add(3);
+  nums.add(2);
+  nums.add(1);
+  nums.add(0);
+
+  assert(nums.IsPalendrome());
 }
