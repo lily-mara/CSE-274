2913a2ce2adb0d3c0f3500dddf4372b16e469b58
Nate Mara <natemara@gmail.com>
2014-10-22 21:25:52 -0400

    add test code for ChainedHashTable

--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -5,8 +5,31 @@
 
 #include "ChainedHashTable.h"
 
+void TestChainedHashTableAdding();
+
 using namespace std;
+using namespace ods;
 
 int main() {
+  TestChainedHashTableAdding();
   return 0;
+}
+
+void TestChainedHashTableAdding() {
+  ChainedHashTable<int> t;
+  t.add(3);
+  t.add(4);
+  t.add(5);
+  t.add(6);
+  t.add(7);
+  t.add(8);
+  t.add(9);
+  t.add(0);
+  t.add(1);
+  t.add(2);
+
+  cout << t.find(0) << endl;
+  cout << t.find(2) << endl;
+  cout << t.find(10) << endl;
+  cout << t.find(100) << endl;
 }
