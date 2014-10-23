648b3c8db81a9a66bf2e3a9fe00b1ddc1fcd8b02
Nate Mara <natemara@gmail.com>
2014-10-22 20:56:52 -0400

    fix indentation in ChainedHashTable

--- assignment3/src/ChainedHashTable.h
+++ assignment3/src/ChainedHashTable.h
@@ -17,29 +17,29 @@ namespace ods {
 template<class T>
 class ChainedHashTable {
 protected:
-	typedef DLList<T> List;
-	T null;
-	array<List> t;
-	int n;
-	int d;
-	int z;
-	static const int w = 32; //sizeof(int)*8;
-	void allocTable(int m);
-	void resize();
-	int hash(T x) {
-		return ((unsigned)(z * hashCode(x))) >> (w-d);
-	}
+  typedef DLList<T> List;
+  T null;
+  array<List> t;
+  int n;
+  int d;
+  int z;
+  static const int w = 32; //sizeof(int)*8;
+  void allocTable(int m);
+  void resize();
+  int hash(T x) {
+    return ((unsigned)(z * hashCode(x))) >> (w-d);
+  }
 
 public:
-	ChainedHashTable();
-	virtual ~ChainedHashTable();
-	bool add(T x);
-	T remove(T x);
-	T find(T x);
-	int size() {
-		return n;
-	}
-	void clear();
+  ChainedHashTable();
+  virtual ~ChainedHashTable();
+  bool add(T x);
+  T remove(T x);
+  T find(T x);
+  int size() {
+    return n;
+  }
+  void clear();
 };
 
 /**
@@ -47,37 +47,37 @@ public:
  */
 template<class T>
 void ChainedHashTable<T>::resize() {
-	d = 1;
-	while (1<<d <= n) d++;
+  d = 1;
+  while (1<<d <= n) d++;
 
-	array<List> newTable(1<<d);
-	for (int i = 0; i < t.length; i++) {
-		for (int j = 0; j < t[i].size(); j++) {
-			T x = t[i].get(j);
-			newTable[hash(x)].add(x);
-		}
-	}
-	t = newTable;
+  array<List> newTable(1<<d);
+  for (int i = 0; i < t.length; i++) {
+    for (int j = 0; j < t[i].size(); j++) {
+      T x = t[i].get(j);
+      newTable[hash(x)].add(x);
+    }
+  }
+  t = newTable;
 }
 
 /*
 template<>
 ChainedHashTable<int>::ChainedHashTable() : t(2)
 {
-	n = 0;
-	d = 1;
-	null = INT_MIN;
-	z = rand() | 1;     // is a random odd integer
+  n = 0;
+  d = 1;
+  null = INT_MIN;
+  z = rand() | 1;     // is a random odd integer
 }
 */
 
 
 template<class T>
 ChainedHashTable<T>::ChainedHashTable() : t(2) {
-	n = 0;
-	d = 1;
-	null = INT_MIN;
-	z = rand() | 1;     // is a random odd integer
+  n = 0;
+  d = 1;
+  null = INT_MIN;
+  z = rand() | 1;     // is a random odd integer
 }
 
 
@@ -89,45 +89,45 @@ ChainedHashTable<T>::~ChainedHashTable() {
 
 template<class T>
 bool ChainedHashTable<T>::add(T x) {
-	if (find(x) != null) return false;
-	if (n+1 > t.length) resize();
-	t[hash(x)].add(x);
-	n++;
-	return true;
+  if (find(x) != null) return false;
+  if (n+1 > t.length) resize();
+  t[hash(x)].add(x);
+  n++;
+  return true;
 }
 
 
 template<class T>
 T ChainedHashTable<T>::remove(T x) {
-	int j = hash(x);
-	for (int i = 0; i < t[j].size(); i++) {
-		T y = t[j].get(i);
-		if (x == y) {
-			t[j].remove(i);
-			n--;
-			return y;
-		}
-	}
-	return null;
+  int j = hash(x);
+  for (int i = 0; i < t[j].size(); i++) {
+    T y = t[j].get(i);
+    if (x == y) {
+      t[j].remove(i);
+      n--;
+      return y;
+    }
+  }
+  return null;
 }
 
 
 template<class T>
 T ChainedHashTable<T>::find(T x) {
-	int j = hash(x);
-	for (int i = 0; i < t[j].size(); i++)
-		if (x == t[j].get(i))
-			return t[j].get(i);
-	return null;
+  int j = hash(x);
+  for (int i = 0; i < t[j].size(); i++)
+    if (x == t[j].get(i))
+      return t[j].get(i);
+  return null;
 }
 
 
 template<class T>
 void ChainedHashTable<T>::clear() {
-	n = 0;
-	d = 1;
-	array<List> b(2);
-	t = b;
+  n = 0;
+  d = 1;
+  array<List> b(2);
+  t = b;
 }
 
 } /* namespace ods */
