e02439bffe59795741cbe7871165323f9cb870e2
Nate Mara <natemara@gmail.com>
2014-10-22 20:57:20 -0400

    fix indentationn

--- assignment3/src/DLList.h
+++ assignment3/src/DLList.h
@@ -13,86 +13,86 @@ namespace ods {
 template<class T>
 class DLList {
 protected:
-	struct Node {
-		T x;
-		Node *prev, *next;
-	};
-	Node dummy;
-	int n;
-	void remove(Node *w);
-	Node* addBefore(Node *w, T x);
-	Node* getNode(int i);
+  struct Node {
+    T x;
+    Node *prev, *next;
+  };
+  Node dummy;
+  int n;
+  void remove(Node *w);
+  Node* addBefore(Node *w, T x);
+  Node* getNode(int i);
 public:
-	DLList();
-	virtual ~DLList();
-	int size() { return n; }
-	T get(int i);
-	T set(int i, T x);
-	virtual void add(int i, T x);
-	virtual void add(T x) { add(size(), x); }
-	virtual T remove(int i);
-	virtual void clear();
+  DLList();
+  virtual ~DLList();
+  int size() { return n; }
+  T get(int i);
+  T set(int i, T x);
+  virtual void add(int i, T x);
+  virtual void add(T x) { add(size(), x); }
+  virtual T remove(int i);
+  virtual void clear();
 };
 
 template<class T>
 DLList<T>::DLList() {
-	dummy.next = &dummy;
-	dummy.prev = &dummy;
-	n = 0;
+  dummy.next = &dummy;
+  dummy.prev = &dummy;
+  n = 0;
 }
 
 template<class T>
 typename DLList<T>::Node* DLList<T>::addBefore(Node *w, T x) {
-	Node *u = new Node;
-	u->x = x;
-	u->prev = w->prev;
-	u->next = w;
-	u->next->prev = u;
-	u->prev->next = u;
-	n++;
-	return u;
+  Node *u = new Node;
+  u->x = x;
+  u->prev = w->prev;
+  u->next = w;
+  u->next->prev = u;
+  u->prev->next = u;
+  n++;
+  return u;
 }
 
 template<class T>
 typename DLList<T>::Node* DLList<T>::getNode(int i) {
-	Node* p;
-	if (i < n / 2) {
-		p = dummy.next;
-		for (int j = 0; j < i; j++)
-			p = p->next;
-	} else {
-		p = &dummy;
-		for (int j = n; j > i; j--)
-			p = p->prev;
-	}
-	return (p);
+  Node* p;
+  if (i < n / 2) {
+    p = dummy.next;
+    for (int j = 0; j < i; j++)
+      p = p->next;
+  } else {
+    p = &dummy;
+    for (int j = n; j > i; j--)
+      p = p->prev;
+  }
+  return (p);
 }
 
 
 template<class T>
 DLList<T>::~DLList() {
-	clear();
+  clear();
 }
 
 template<class T>
 void DLList<T>::clear() {
-	Node *u = dummy.next;
-	while (u != &dummy) {
-		Node *w = u->next;
-		delete u;
-		u = w;
-	}
-	n = 0;
+  Node *u = dummy.next;
+  while (u != &dummy) {
+    Node *w = u->next;
+    delete u;
+    u = w;
+  }
+  n = 0;
 }
 
 
 
 template<class T>
 void DLList<T>::remove(Node *w) {
-	w->prev->next = w->next;
-	w->next->prev = w->prev;
-	delete w;
-	n--;
+  w->prev->next = w->next;
+  w->next->prev = w->prev;
+  delete w;
+  n--;
 }
 
 
@@ -103,10 +103,10 @@ T DLList<T>::get(int i) {
 
 template<class T>
 T DLList<T>::set(int i, T x) {
-	Node* u = getNode(i);
-	T y = u->x;
-	u->x = x;
-	return y;
+  Node* u = getNode(i);
+  T y = u->x;
+  u->x = x;
+  return y;
 }
 
 template<class T>
@@ -116,10 +116,10 @@ void DLList<T>::add(int i, T x) {
 
 template<class T>
 T DLList<T>::remove(int i) {
-	Node *w = getNode(i);
-	T x = w->x;
-	remove(w);
-	return x;
+  Node *w = getNode(i);
+  T x = w->x;
+  remove(w);
+  return x;
 }
 
 
