7c8fe655e3659dcc0ba7cf6166df6731b2f490cc
Nate Mara <natemara@gmail.com>
2014-11-06 18:57:31 -0500

    add assignment 4 starter code

---
+++ assignment4/src/test.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Nate Mara
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Test code for the Binary Search Tree classes
+//============================================================================
+
+#include <iostream>
+
+#include "BinarySearchTree.h"
+#include "BinaryTree.h"
+
+using namespace std;
+
+int main() {
+  return 0;
+}
