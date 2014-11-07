ad64503e1c3c9e495c43d44755908e1a35346b3b
Nate Mara <natemara@gmail.com>
2014-11-06 19:12:21 -0500

    implement and test BinaryTree::height2

--- assignment4/src/test.cpp
+++ assignment4/src/test.cpp
@@ -12,7 +12,21 @@
 #include "BinaryTree.h"
 
 using namespace std;
+using namespace ods;
+
+void TestBinaryTreeSize2();
 
 int main() {
+  TestBinaryTreeSize2();
   return 0;
+}
+
+void TestBinaryTreeSize2() {
+  BinarySearchTree<BSTNode1<int>, int> b;
+
+  for(int i = 0; i < 1000; i++) {
+    b.add(rand() % 100000);
+  }
+
+  assert(b.height2(b.getRoot()) == b.size());
 }
