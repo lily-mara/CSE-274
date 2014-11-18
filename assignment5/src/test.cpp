0ee6c487063be1859317d92e8edfe8247213d277
Nate Mara <natemara@gmail.com>
2014-11-18 14:10:32 -0500

    write structure for testing program

--- assignment5/src/test.cpp
+++ assignment5/src/test.cpp
@@ -5,15 +5,58 @@
 #include "BinarySearchTree.h"
 #include "RedBlackTree.h"
 
+#define TESTS 100
+
 using namespace std;
 using namespace ods;
 
-int main() {
-  DataStructureTester<BinarySearchTree<BSTNode1<int>, int> > test;
-  cout << test.DoRandomAdd(10000) << endl;
-  cout << test.DoSequentialAdd(0, 1000000, 100) << endl;
-  cout << test.DoSequentialRemove(0, 1000000, 100) << endl;
+template<class T>
+void TestOneDataStructure(DataStructureTester<T>);
+void TestAllDataStructures();
+void TestBinarySearchTree();
+void TestRedBlackTree();
+void TestLinearHashTable();
+void TestChainedHashTable();
 
+int main() {
+  TestAllDataStructures();
 
   return 0;
+}
+
+void TestBinarySearchTree() {
+  DataStructureTester<BinarySearchTree<BSTNode1<int>, int> > test;
+  TestOneDataStructure(test);
+}
+
+void TestRedBlackTree() {
+
+}
+
+void TestChainedHashTable() {
+
+}
+
+void TestLinearHashTable() {
+
+}
+
+void TestAllDataStructures() {
+  for (int i = 0; i < TESTS; i++) {
+    TestBinarySearchTree();
+    TestChainedHashTable();
+    TestRedBlackTree();
+    TestLinearHashTable();
+  }
+}
+
+template<class T>
+void TestOneDataStructure(DataStructureTester<T> tester) {
+  tester.DoSequentialAdd(0, 100000, 1);
+  tester.DoRandomAdd(100000);
+  tester.DoSequentialFind(0, 100000, 1);
+  tester.DoSequentialRemove(0, 100000, 1);
+
+  // Do random finds on 100,000 items
+  // Do random removes on 100,000 items
 }
