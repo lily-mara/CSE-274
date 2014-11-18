b380ccd152160a5e3367382e7d6d8869b84ad6fa
Nate Mara <natemara@gmail.com>
2014-11-18 13:39:49 -0500

    implement DataStructureTester::DoSequentialFind and DataStructureTester::TotalTime

--- assignment5/src/DataStructureTester.h
+++ assignment5/src/DataStructureTester.h
@@ -8,12 +8,20 @@ class DataStructureTester {
 protected:
   H list;
 
+private:
+  int TotalTime(int);
 public:
   int DoRandomAdd(int);
   int DoSequentialAdd(int, int, int);
   int DoSequentialRemove(int, int, int);
+  int DoSequentialFind(int, int, int);
   DataStructureTester();
 };
+
+template<class T> inline
+int DataStructureTester<T>::TotalTime(int start_time) {
+  return (clock() - start_time) / (CLOCKS_PER_SEC / 1000);
+}
 
 template<class T>
 DataStructureTester<T>::DataStructureTester() {
@@ -21,25 +29,33 @@ DataStructureTester<T>::DataStructureTester() {
 }
 
 template<class T>
+int DataStructureTester<T>::DoSequentialFind(int start, int end, int step) {
+  clock_t start_time = clock();
+  for (int i = start; i < end; i += step) {
+    list.find(i);
+  }
+
+  return TotalTime(start_time);
+}
+
+template<class T>
 int DataStructureTester<T>::DoSequentialRemove(int start, int end, int step) {
   clock_t start_time = clock();
-  for (int i = start; i < end; i+=step) {
+  for (int i = start; i < end; i += step) {
     list.remove(i);
   }
-  clock_t end_time = clock();
 
-  return (end_time - start_time) / (CLOCKS_PER_SEC / 1000);
+  return TotalTime(start_time);
 }
 
 template<class T>
 int DataStructureTester<T>::DoSequentialAdd(int start, int end, int step) {
   clock_t start_time = clock();
-  for (int i = start; i < end; i+=step) {
+  for (int i = start; i < end; i += step) {
     list.add(i);
   }
-  clock_t end_time = clock();
 
-  return (end_time - start_time) / (CLOCKS_PER_SEC / 1000);
+  return TotalTime(start_time);
 }
 
 template<class T>
@@ -48,9 +64,8 @@ int DataStructureTester<T>::DoRandomAdd(int elements_to_add) {
   for (int i = 0; i < elements_to_add; i++) {
     list.add(rand() % INT_MAX);
   }
-  clock_t end_time = clock();
 
-  return (end_time - start_time) / (CLOCKS_PER_SEC / 1000);
+  return TotalTime(start_time);
 }
 
 }
