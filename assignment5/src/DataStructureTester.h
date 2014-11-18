331e7c27699cd6d0ece2643f3a66d5230f12d005
Nate Mara <natemara@gmail.com>
2014-11-18 13:18:39 -0500

    implement DataStructureTester::AddRandom

--- assignment5/src/DataStructureTester.h
+++ assignment5/src/DataStructureTester.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 namespace ods {
 
@@ -8,10 +9,24 @@ protected:
   H list;
 
 public:
-  void AddRandom(int i) {
-    list.add(i);
-    //std::cout << "find " << list.find(i) << std::endl;
-  }
+  int AddRandom(int);
+  DataStructureTester();
 };
+
+template<class T>
+DataStructureTester<T>::DataStructureTester() {
+  srand(time(0));
+}
+
+template<class T>
+int DataStructureTester<T>::AddRandom(int elements_to_add) {
+  clock_t start_time = clock();
+  for (int i = 0; i < elements_to_add; i++) {
+    list.add(rand() % INT_MAX);
+  }
+  clock_t end_time = clock();
+
+  return (end_time - start_time) / (CLOCKS_PER_SEC / 1000);
+}
 
 }
